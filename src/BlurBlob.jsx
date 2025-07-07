be4f93185dc90d76af72b5bf774a7c5a56e7d63d
mehedi1022 <mahdibinalamgir@gmail.com>
2025-07-07 15:56:36 +0600

    added github link in navbar

---
+++ src/BlurBlob.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const BlurBlob = () => {
+    return (
+        <div className="absolute" style={{
+            top: top,
+            left: left,
+            width: width,
+            height: height,
+            transform: 'translate'
+        }}>
+            
+        </div>
+    );
+};
+
+export default BlurBlob;
