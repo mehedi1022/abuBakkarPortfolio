ca1f2126e111b9bf8446867df74f2768ce19fe0a
mehedi1022 <mahdibinalamgir@gmail.com>
2025-07-08 15:10:15 +0600

    added skills section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,64 @@
 import React from 'react';
+import { Typewriter } from 'react-simple-typewriter';
+import profileImage from '../../assets/profile1.jpg'
+import ReactParallaxTilt from 'react-parallax-tilt';
 
 const About = () => {
     return (
-        <div>
-            
-        </div>
+        <section id="about" className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32">
+            <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+                {/* Left Side  */}
+                <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
+                    {/* Greeting  */}
+                    <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold text-white mb-2 leading-tight">
+                        Hi, I am
+                    </h1>
+                    {/* Name  */}
+                    <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
+                        Abu Bakkar
+                    </h2>
+                    {/* Skills heading with typing effect  */}
+                    <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold text-prim leading-tight">
+                        <span className="text-white">I am a </span>
+                        <span className="text-prim">
+                            <Typewriter
+                                words={['DevOps Engineer', 'Cloud Engineer', 'Django Developer', 'Flutter Developer']}
+                                loop
+                                cursor
+                                cursorStyle="_"
+                                typeSpeed={100}
+                                deleteSpeed={50}
+                                delaySpeed={2000}
+                            />
+                        </span>
+                    </h3>
+                    {/* About me paragraph */}
+                    <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
+                        Aspiring Cloud Engineer with hands-on experience in AWS, Docker, and Terraform. Skilled in Python backend
+                        and passionate about automating cloud infrastructure. Currently building and deploying cloud projects, with a
+                        strong focus on open-source contributions and continuous improvement.
+                    </p>
+                    {/* Resume Button  */}
+                    <a href="https://drive.google.com/file/d/1xj301PJvt_fJ30UK61dczHW_v-QWbl_6/view"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+                        style={{
+                            background: 'linear-gradient(90deg, #8245ec, #a855f7)',
+                            boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
+                        }}
+                    >
+                        DOWNLOAD CV
+                    </a>
+                </div>
+                {/* Right Side  */}
+                <div className="md:w-1/2 flex justify-center md:justify-end">
+                <ReactParallaxTilt className="w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full" tiltMaxAngleX={20} tiltMaxAngleY={20} perspective={1000} scale={1.05} transitionSpeed={1000} gyroscope={true}>
+                <img src={profileImage} alt="Abu Bakkar" className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130, 69, 236, 0.5)]" />
+                </ReactParallaxTilt>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/constants.js
+++ src/constants.js
@@ -1,37 +1,27 @@
 // Skills Section Logo's
 import htmlLogo from './assets/tech_logo/html.png';
 import cssLogo from './assets/tech_logo/css.png';
-import sassLogo from './assets/tech_logo/sass.png';
-import javascriptLogo from './assets/tech_logo/javascript.png';
-import reactjsLogo from './assets/tech_logo/reactjs.png';
-import angularLogo from './assets/tech_logo/angular.png';
-import reduxLogo from './assets/tech_logo/redux.png';
-import nextjsLogo from './assets/tech_logo/nextjs.png';
-import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
-import gsapLogo from './assets/tech_logo/gsap.png';
-import materialuiLogo from './assets/tech_logo/materialui.png';
-import bootstrapLogo from './assets/tech_logo/bootstrap.png';
-import springbootLogo from './assets/tech_logo/springboot.png';
-import nodejsLogo from './assets/tech_logo/nodejs.png';
-import expressjsLogo from './assets/tech_logo/express.png';
+import docker from './assets/tech_logo/docker.png';
+import aws from './assets/tech_logo/aws.png';
+import Terraform from './assets/tech_logo/terraform.png';
+import  Ansible from './assets/tech_logo/Ansible.png';
+import  GitHubActions from './assets/tech_logo/GitHubActions.png';
+import  Jenkins from './assets/tech_logo/jenkins.png';
+
+
 import mysqlLogo from './assets/tech_logo/mysql.png';
 import mongodbLogo from './assets/tech_logo/mongodb.png';
-import firebaseLogo from './assets/tech_logo/firebase.png';
+import Django from './assets/tech_logo/dj.png'
+
 import cLogo from './assets/tech_logo/c.png';
 import cppLogo from './assets/tech_logo/cpp.png';
 import javaLogo from './assets/tech_logo/java.png';
 import pythonLogo from './assets/tech_logo/python.png';
-import typescriptLogo from './assets/tech_logo/typescript.png';
+
 import gitLogo from './assets/tech_logo/git.png';
 import githubLogo from './assets/tech_logo/github.png';
 import vscodeLogo from './assets/tech_logo/vscode.png';
-import postmanLogo from './assets/tech_logo/postman.png';
-import mcLogo from './assets/tech_logo/mc.png';
-import figmaLogo from './assets/tech_logo/figma.png';
-import netlifyLogo from './assets/tech_logo/netlify.png';
-import vercelLogo from './assets/tech_logo/vercel.png';
-import postgreLogo from './assets/tech_logo/postgre.png';
-import csharpLogo from './assets/tech_logo/csharp.png';
+
 
 // Experience Section Logo's
 import webverseLogo from './assets/company_logo/webverse_logo.png';
@@ -57,32 +47,25 @@ import removebgLogo from './assets/work_logo/remove_bg.png';
 
 export const SkillsInfo = [
   {
-    title: 'Frontend',
+    title: 'Cloud & DevOps',
     skills: [
-      { name: 'HTML', logo: htmlLogo },
-      { name: 'CSS', logo: cssLogo },
-      { name: 'SASS', logo: sassLogo },
-      { name: 'JavaScript', logo: javascriptLogo },
-      { name: 'React JS', logo: reactjsLogo },
-      { name: 'Angular', logo: angularLogo },
-      { name: 'Redux', logo: reduxLogo },
-      { name: 'Next JS', logo: nextjsLogo },
-      { name: 'Tailwind CSS', logo: tailwindcssLogo },
-      { name: 'GSAP', logo: gsapLogo },
-      { name: 'Material UI', logo: materialuiLogo },
-      { name: 'Bootstrap', logo: bootstrapLogo },
+      { name: 'Docker', logo: docker },
+      { name: 'AWS', logo: aws },
+      { name: 'Terraform', logo: Terraform },
+      { name: 'Ansible', logo: Ansible },
+      { name: 'GitHub Actions', logo: GitHubActions },
+      { name: 'Jenkins', logo: Jenkins },
+      
     ],
   },
   {
     title: 'Backend',
     skills: [
-      { name: 'Springboot', logo: springbootLogo },
-      { name: 'Node JS', logo: nodejsLogo },
-      { name: 'Express JS', logo: expressjsLogo },
+     
       { name: 'MySQL', logo: mysqlLogo },
       { name: 'MongoDB', logo: mongodbLogo },
-      { name: 'Firebase', logo: firebaseLogo },
-      { name: 'PostgreSQL', logo: postgreLogo },
+      { name: 'Django', logo: Django },
+     
     ],
   },
   {
@@ -92,9 +75,7 @@ export const SkillsInfo = [
       { name: 'C++', logo: cppLogo },
       { name: 'Java', logo: javaLogo },
       { name: 'Python', logo: pythonLogo },
-      { name: 'C-Sharp', logo: csharpLogo },
-      { name: 'JavaScript', logo: javascriptLogo },
-      { name: 'TypeScript', logo: typescriptLogo },
+     
     ],
   },
   {
@@ -103,11 +84,7 @@ export const SkillsInfo = [
       { name: 'Git', logo: gitLogo },
       { name: 'GitHub', logo: githubLogo },
       { name: 'VS Code', logo: vscodeLogo },
-      { name: 'Postman', logo: postmanLogo },
-      { name: 'Compass', logo: mcLogo },
-      { name: 'Vercel', logo: vercelLogo },
-      { name: 'Netlify', logo: netlifyLogo },
-      { name: 'Figma', logo: figmaLogo },
+      
     ],
   },
 ];
